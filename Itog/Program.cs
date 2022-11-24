2a9f83a64f3aee9f44ec15733de9ac3963fc7d09
SergeiGorkun <sergeigorkun1992@yandex.ru>
2022-11-24 23:02:06 +0600

    create program

---
+++ Itog/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] array = { "123", ":-)", "Monaco", "Ufa", "Moskow", "Ural" };
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(array);
+
+
+string [] ArraystringsThreeCharacters (string [] arr){
+    string [] newArray = {};
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++){
+        if(arr[i].Length <= 3){
+           Array.Resize(ref newArray, j+1);
+           newArray[j] = arr[i];
+           j++;
+        }
+    }
+    return newArray;
+}
+array = ArraystringsThreeCharacters (array);
+PrintArray(array);
